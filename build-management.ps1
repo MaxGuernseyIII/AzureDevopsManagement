29c3c7f04c06073115faebce5798e7991bf0319d
MaxGuernseyIII <max@hexsw.com>
2020-02-02 13:06:02 -0800

    Adding ability to pause or unpause a pipeline.

--- build-management.ps1
+++ build-management.ps1
@@ -1,4 +1,8 @@
-
+enum BuildQueueStatus {
+  disabled
+  enabled
+  paused
+}
 
 function GetSystemToken {
   $PAT = ":$env:SYSTEM_PAT"
@@ -40,8 +44,29 @@ function Remove-AzureDevOpsBuilds {
 
   foreach ($BuildUrl in $BuildUrls) {
     Invoke-RestMethod -Uri $BuildUrl -Method Delete -Headers @{
-      'Authorization' = "Basic $token";
+      'Authorization' = "Basic $Token";
       'Content-Type' = "application/json"
     }
   }
+}
+
+function Set-AzureDevOpsPipelineQueueStatus {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory)][string]$DefinitionId,
+    [Parameter(Mandatory)][BuildQueueStatus]$NewStatus
+  )
+
+  $Token = GetSystemToken
+  $Url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/build/definitions/$($DefinitionId)/?api-version=5.1"
+
+  $Headers = @{
+    'Authorization' = "Basic $Token";
+    'Content-Type' = "application/json"
+  }
+
+  $Pipeline = Invoke-RestMethod -Uri $Url -Method Get -Headers $Headers
+  $Pipeline.queueStatus = "$NewStatus"
+
+  Invoke-RestMethod -Uri $Url -Method Put -Body ($Pipeline | ConvertTo-Json) -Headers $Headers
 }
